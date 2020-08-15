99f89a54173454dcc5cd3872cbb3362c1143711c
Paul Addai <pauladdai19@augustana.edu>
2020-08-15 01:07:26 -0500

    All is set to go

--- forloop.cpp
+++ forloop.cpp
@@ -21,6 +21,7 @@ int main(){
         if(i <= 9){cout<<value_names[i-1]<<endl;}
         else if(i % 2 == 0) {cout<<"even"<<endl;}
         else{cout<<"old"<<endl;} //this section
+        //all set
 
         //hello I made some changes
 
