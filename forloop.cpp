e9b3c60bbb36488b0471d3451c91b4f9b5abb17e
Paul Addai <pauladdai19@augustana.edu>
2020-08-15 00:42:17 -0500

    almost done

---
+++ forloop.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main(){
+
+    //variable and inputs
+    int val_a,val_b;
+    cin>>val_a;
+    cin>>val_b;
+
+    //array
+    vector<string> value_names = {"one","two","three","four","five","six","seven","eight","nine"};
+
+    //for loop
+    for (int i = val_a; i <= val_b; i++)
+    {
+        //condition
+        if(i <= 9){cout<<i<<value_names[i-1]<<endl;}
+        else if(i % 2 == 0) {cout<<i<<"even"<<endl;}
+        else{cout<<"old"<<endl;}
+        
+    }
+    
+    return 0;
+}
